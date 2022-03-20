dd0b323b94d3c0e047ca7553266aee557a67bf19
svveden <55561624+svveden@users.noreply.github.com>
2022-03-20 11:51:13 -0500

    v 0.2.2
    Added ranges

--- fctr.c
+++ fctr.c
@@ -8,8 +8,8 @@
 
 // PROTOTYPES
 
-void delete_folders(char *num, char *argv_name);
-void create_folders(char *num, char *argv_name);
+void delete_folders(char *num, char *argv_name, int range);
+void create_folders(char *num, char *argv_name, int range);
 bool is_number(char *num);
 
 // 
@@ -17,6 +17,7 @@ bool is_number(char *num);
 
 int main(int argc, char *argv[]){
 	int num;
+	int range_min = 1;
 	if(argc < 4){
 		fprintf(stderr, " ----------------------------------------------------------------------\n");
 		fprintf(stderr, "| This tool creates a series of folders from 01 to n with a given name |\n");
@@ -24,6 +25,17 @@ int main(int argc, char *argv[]){
 		fprintf(stderr, "| Usage: fctr -flag number_of_folders name_of_folders                  |\n");
 		fprintf(stderr, " ----------------------------------------------------------------------\n");
 		exit(EXIT_FAILURE);
+	}
+	if(argc == 5){
+		if(!is_number(argv[4])){
+			fprintf(stderr, "Number of folders invalid!\n");
+			exit(EXIT_FAILURE);
+		}
+		if(atoi(argv[4]) == 0){
+			range_min = 1;
+		} else{
+			range_min = atoi(argv[4]);
+		}
 	}
 	if(argv[1][0] != '-'){
 		fprintf(stderr, "Flag symbol incorrect; did you forget '-' ?\n");
@@ -38,10 +50,10 @@ int main(int argc, char *argv[]){
 		exit(EXIT_FAILURE);
 	}
 	if(argv[1][1] == 'd'){
-		delete_folders(argv[2], argv[3]);
+		delete_folders(argv[2], argv[3], range_min);
 	}
 	if(argv[1][1] == 'c'){
-		create_folders(argv[2], argv[3]);
+		create_folders(argv[2], argv[3], range_min);
 	}
 	if(argv[1][1] != 'c' && argv[1][1] != 'd'){
 		fprintf(stderr, "Unknown flag '%c'\nFlags:\n-c\t\tCreates folders.\n-d\t\tDeletes folders.\n", argv[1][1]);
@@ -49,12 +61,12 @@ int main(int argc, char *argv[]){
 	return 0;
 }
 
-void delete_folders(char *num, char *argv_name){
+void delete_folders(char *num, char *argv_name, int range){
 	char name[25];
 	char buffer[25];
 	int deleted = 0;
 	int err;
-	for(int i = 1; i <= atoi(num); i++){
+	for(int i = range; i <= atoi(num); i++){
 		sprintf(buffer, "_%.2d", i);
 		strcpy(name, argv_name);
 		strcat(name, buffer);
@@ -72,12 +84,12 @@ void delete_folders(char *num, char *argv_name){
 	return;
 }
 
-void create_folders(char *num, char *argv_name){
+void create_folders(char *num, char *argv_name, int range){
 	char name[25];
 	char buffer[25];
 	int created = 0;
 	int err = 0;
-	for(int i = 1; i <= atoi(num); i++){
+	for(int i = range; i <= atoi(num); i++){
 		sprintf(buffer, "_%.2d", i);
 		strcpy(name, argv_name);
 		strcat(name, buffer);
