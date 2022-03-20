c344d7c1a92240967da9d7ace18cfadb571432e5
svveden <55561624+svveden@users.noreply.github.com>
2022-03-20 00:06:29 -0500

    added error checking

--- fctr.c
+++ fctr.c
@@ -2,12 +2,34 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+bool is_number(char *num){
+	
+	if(num[0] == '-'){
+			fprintf(stderr, "You can't create NEGATIVE folders, can you?\n");
+			exit(EXIT_FAILURE);
+	}
+	for(int i = 0; i < strlen(num); i++){
+		if(isdigit(num[i])){
+			i++;
+		} else{
+			return false;
+		}
+	} 
+	return true;
+}
 
 int main(int argc, char *argv[]){
 	int num;
-	if(argc < 2){
+	if(argc < 3){
 		fprintf(stderr, "This tool creates a series of folders from 01 to n with a given name\nExample: folder_01, folder_02, folder_0n\n");
 		fprintf(stderr, "Usage: fctr number_of_folders name_of_folders\n");
+		exit(EXIT_FAILURE);
+	}
+	if(!is_number(argv[1])){
+		fprintf(stderr, "Number of folders invalid!\n");
 		exit(EXIT_FAILURE);
 	}
 	for(int i = 1; i <= atoi(argv[1]); i++){
