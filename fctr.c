5c70ed0ffd1b562f3e7f32af2f8e423f96096582
svveden <55561624+svveden@users.noreply.github.com>
2022-03-21 12:16:44 -0500

    v 0.3
    Added -m mode for folder matching with files

--- fctr.c
+++ fctr.c
@@ -5,12 +5,17 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <fcntl.h>
 
 // PROTOTYPES
 
 void delete_folders(char *num, char *argv_name, int range);
 void create_folders(char *num, char *argv_name, int range);
 bool is_number(char *num);
+char *remove_extension(char a[]);
+void folder_matching(void);
+
 
 // 
 
@@ -18,11 +23,13 @@ bool is_number(char *num);
 int main(int argc, char *argv[]){
 	int num;
 	int range_min = 1;
-	if(argc < 4){
+	if(argc < 4 && strcmp(argv[1], "-m") != 0 && strcmp(argv[1], "-?") != 0){
 		fprintf(stderr, " ----------------------------------------------------------------------\n");
 		fprintf(stderr, "| This tool creates a series of folders from 01 to n with a given name |\n");
+		fprintf(stderr, "| and can also sort files into folders matching their names.           |\n");
 		fprintf(stderr, "| Example: folder_01, folder_02, folder_n                              |\n");
 		fprintf(stderr, "| Usage: fctr -flag number_of_folders name_of_folders                  |\n");
+		fprintf(stderr, "| Type: 'fctr -?' to see flags                                         |\n");
 		fprintf(stderr, " ----------------------------------------------------------------------\n");
 		exit(EXIT_FAILURE);
 	}
@@ -37,28 +44,78 @@ int main(int argc, char *argv[]){
 			range_min = atoi(argv[4]);
 		}
 	}
+	if(argc == 4){
+		if(!is_number(argv[2])){
+			fprintf(stderr, "Number of folders invalid!\n");
+			exit(EXIT_FAILURE);
+		}
+		if(strlen(argv[3]) > 250){
+			fprintf(stderr, "The maximum folder name length is generally 255, this could exceed that\n");
+			exit(EXIT_FAILURE);
+		}
+	}
 	if(argv[1][0] != '-'){
 		fprintf(stderr, "Flag symbol incorrect; did you forget '-' ?\n");
 		exit(EXIT_FAILURE);
 	}
-	if(!is_number(argv[2])){
-		fprintf(stderr, "Number of folders invalid!\n");
-		exit(EXIT_FAILURE);
-	}
-	if(strlen(argv[3]) > 250){
-		fprintf(stderr, "The maximum folder name length is generally 255, this could exceed that\n");
-		exit(EXIT_FAILURE);
-	}
-	if(argv[1][1] == 'd'){
-		delete_folders(argv[2], argv[3], range_min);
-	}
-	if(argv[1][1] == 'c'){
-		create_folders(argv[2], argv[3], range_min);
-	}
-	if(argv[1][1] != 'c' && argv[1][1] != 'd'){
-		fprintf(stderr, "Unknown flag '%c'\nFlags:\n-c\t\tCreates folders.\n-d\t\tDeletes folders.\n", argv[1][1]);
+	
+	switch(argv[1][1]){
+		case 'd':
+			delete_folders(argv[2], argv[3], range_min);
+			break;
+		case 'c':
+			create_folders(argv[2], argv[3], range_min);
+			break;
+		case 'm':
+			folder_matching();
+			break;
+		case '?':
+			fprintf(stderr, "\nFlags:\n-c\t\tCreates folders.\n-d\t\tDeletes folders.\n-m\t\tMatching mode.\n\n");
+			exit(EXIT_FAILURE);
+			break;
+		default:
+			fprintf(stderr, "Unknown flag '%c'\nFlags:\n-c\t\tCreates folders.\n-d\t\tDeletes folders.\n\n", argv[1][1]);
+			exit(EXIT_FAILURE);
 	}
 	return 0;
+}
+
+char *remove_extension(char a[]){
+	for(int i = 0; i < strlen(a); i++){
+		if(a[i] == '.'){
+			a[i] = '\0';
+			return a;
+		}
+	}
+	return a;
+}
+
+void folder_matching(void){
+	DIR *d;
+	struct dirent *dir;
+	d = opendir(".");
+	if(d){
+		while((dir = readdir(d)) != NULL){
+			char str2[255] = "./";
+			char str1[255] = "./";
+			char file_name[255];
+			char *folder_name;
+				strncpy(file_name, dir->d_name, strlen(dir->d_name));
+				file_name[strlen(dir->d_name)] = '\0';
+				folder_name = remove_extension(dir->d_name);
+				mkdir(folder_name, 0755);
+				strncat(str2, folder_name, strlen(folder_name));
+				strncat(str2, "/", 1);
+				strncat(str2, file_name, strlen(file_name));
+				strncat(str1, file_name, strlen(file_name));
+				printf("%s\n", str1);
+				printf("%s\n", str2);
+				rename(str1, str2);
+			//}
+		}
+	}
+	closedir(d);
+	return;
 }
 
 void delete_folders(char *num, char *argv_name, int range){
